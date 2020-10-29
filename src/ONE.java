25ba963278c8a807cb5b2a89bb71571e0ae6cf69
sahakyan-tigran <62710477+sahakyan-tigran@users.noreply.github.com>
2020-10-29 12:14:17 +0400

    Adding homework

---
+++ src/ONE.java
@@ -0,0 +1,20 @@
+public class ONE {
+    public static void main(String[] args) {
+        byte x = 5;
+        System.out.println(x);
+        short y = 15;
+        System.out.println(y);
+        int z = 115;
+        System.out.println(z);
+        long w = 1115L;
+        System.out.println(w);
+        float c = 14.3F;
+        System.out.println(c);
+        double d = 456.2;
+        System.out.println(d);
+        char f = '!';
+        System.out.println(f);
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
